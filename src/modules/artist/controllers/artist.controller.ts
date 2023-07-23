1cbfce6e2d89aefdc7eb33287b5cf4b56490ae61
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-07-23 18:10:28 +0300

    feat: add cud artist

--- src/modules/artist/controllers/artist.controller.ts
+++ src/modules/artist/controllers/artist.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { ArtistService } from '../services/artist.service';
+import { CreateArtistDto } from '../dto/create-artist.dto';
 
 @Controller('artist')
 export class ArtistController {
@@ -13,5 +22,20 @@ export class ArtistController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.artistService.findArtistById(id);
+  }
+
+  @Post()
+  create(@Body() body: CreateArtistDto) {
+    return this.artistService.createArtist(body);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() body: CreateArtistDto) {
+    return this.artistService.updateArtistInfo(id, body);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.artistService.deleteArtist(id);
   }
 }
--- src/modules/artist/services/artist.service.ts
+++ src/modules/artist/services/artist.service.ts
@@ -5,6 +5,8 @@ import {
 } from '@nestjs/common';
 import { ArtistRepository } from '../repositories/artist.repository';
 import { isUUID } from '@nestjs/common/utils/is-uuid';
+import { CreateArtistDto } from '../dto/create-artist.dto';
+import { UpdateArtistDto } from '../dto/update-artist.dto';
 
 @Injectable()
 export class ArtistService {
@@ -21,5 +23,28 @@ export class ArtistService {
     if (!artist) throw new NotFoundException(`Artist with id ${id} not found`);
 
     return artist;
+  }
+
+  createArtist(artistData: CreateArtistDto) {
+    if (!artistData.name || !artistData.grammy)
+      throw new BadRequestException('Missing required fields');
+
+    return this.artistRepository.createArtist(artistData);
+  }
+
+  updateArtistInfo(id: string, artistData: UpdateArtistDto) {
+    if (!isUUID(id)) throw new BadRequestException(`Invalid artist id ${id}`);
+
+    const artist = this.artistRepository.findArtistById(id);
+    if (!artist) throw new NotFoundException('Artist not found');
+
+    return this.artistRepository.updateArtistInfo(id, artistData);
+  }
+
+  deleteArtist(id: string) {
+    if (!isUUID(id)) throw new BadRequestException('Invalid artist ID');
+
+    const result = this.artistRepository.deleteArtist(id);
+    if (!result) throw new NotFoundException('Artist not found');
   }
 }
