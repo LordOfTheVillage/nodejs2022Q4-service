1cb9f92ff83ffa85d0954280508d0321f1743fd1
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-07-25 20:51:59 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from './common/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   app.useGlobalPipes(new ValidationPipe());
   const port = Number(process.env.PORT) || 4000;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
 }
 
 bootstrap();
