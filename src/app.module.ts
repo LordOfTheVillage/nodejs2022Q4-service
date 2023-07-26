12272c6bbac813118c1e1dba5d27ae7c46b5dc59
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-07-26 16:29:04 +0300

    feat: link app & db in docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,20 @@ import { AlbumModule } from './modules/album/album.module';
 import { StoreModule } from './modules/store/store.module';
 import { TrackModule } from './modules/track/track.module';
 import { FavoritesModule } from './modules/favorites/favorites.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config) => ({
+        type: 'postgres',
+        url: config.getOrThrow('DATABASE_URL'),
+      }),
+    }),
     UserModule,
     AlbumModule,
     StoreModule,
