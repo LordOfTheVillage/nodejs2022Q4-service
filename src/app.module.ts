558ef254b24d9b82501841f7b1e25b53724de716
LordOfTheVillage <maksim.kyl285@gmail.com>
2023-07-30 10:14:01 +0300

    feat: add images scan

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,10 @@ import { AlbumModule } from './modules/album/album.module';
 import { StoreModule } from './modules/store/store.module';
 import { TrackModule } from './modules/track/track.module';
 import { FavoritesModule } from './modules/favorites/favorites.module';
-import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
     PrismaModule,
     UserModule,
     AlbumModule,
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { setupSwagger } from './common/swagger';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  setupSwagger(app);
   app.useGlobalPipes(new ValidationPipe());
   const port = Number(process.env.PORT) || 4000;
   await app.listen(port);
